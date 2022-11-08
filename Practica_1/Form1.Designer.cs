09ceb14c472ba4e238ad0ffd9cfe9f7f812c393b
yaelJesus <yael_jrm@hotmail.es>
2022-11-07 18:40:26 -0600

    Agregar archivos de proyecto.

---
+++ Practica_1/Form1.Designer.cs
@@ -0,0 +1,374 @@
+﻿
+namespace Practica_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDato1 = new System.Windows.Forms.TextBox();
+            this.txtDato2 = new System.Windows.Forms.TextBox();
+            this.rbSuma = new System.Windows.Forms.RadioButton();
+            this.rbResta = new System.Windows.Forms.RadioButton();
+            this.rbMulti = new System.Windows.Forms.RadioButton();
+            this.rbDivicion = new System.Windows.Forms.RadioButton();
+            this.rbMin = new System.Windows.Forms.RadioButton();
+            this.rbMax = new System.Windows.Forms.RadioButton();
+            this.rbRC = new System.Windows.Forms.RadioButton();
+            this.rbPow = new System.Windows.Forms.RadioButton();
+            this.rbRedondeo = new System.Windows.Forms.RadioButton();
+            this.rbPI = new System.Windows.Forms.RadioButton();
+            this.gbBasicas = new System.Windows.Forms.GroupBox();
+            this.gbAvanzadas = new System.Windows.Forms.GroupBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.gbBasicas.SuspendLayout();
+            this.gbAvanzadas.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Valor 2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 211);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 235);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 3;
+            // 
+            // txtDato1
+            // 
+            this.txtDato1.BackColor = System.Drawing.Color.BlueViolet;
+            this.txtDato1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtDato1.Location = new System.Drawing.Point(61, 11);
+            this.txtDato1.Name = "txtDato1";
+            this.txtDato1.Size = new System.Drawing.Size(100, 13);
+            this.txtDato1.TabIndex = 4;
+            // 
+            // txtDato2
+            // 
+            this.txtDato2.BackColor = System.Drawing.Color.BlueViolet;
+            this.txtDato2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtDato2.Location = new System.Drawing.Point(61, 40);
+            this.txtDato2.Name = "txtDato2";
+            this.txtDato2.Size = new System.Drawing.Size(100, 13);
+            this.txtDato2.TabIndex = 5;
+            // 
+            // rbSuma
+            // 
+            this.rbSuma.AutoSize = true;
+            this.rbSuma.Location = new System.Drawing.Point(6, 19);
+            this.rbSuma.Name = "rbSuma";
+            this.rbSuma.Size = new System.Drawing.Size(52, 17);
+            this.rbSuma.TabIndex = 6;
+            this.rbSuma.TabStop = true;
+            this.rbSuma.Text = "Suma";
+            this.rbSuma.UseVisualStyleBackColor = true;
+            // 
+            // rbResta
+            // 
+            this.rbResta.AutoSize = true;
+            this.rbResta.Location = new System.Drawing.Point(6, 46);
+            this.rbResta.Name = "rbResta";
+            this.rbResta.Size = new System.Drawing.Size(53, 17);
+            this.rbResta.TabIndex = 7;
+            this.rbResta.TabStop = true;
+            this.rbResta.Text = "Resta";
+            this.rbResta.UseVisualStyleBackColor = true;
+            // 
+            // rbMulti
+            // 
+            this.rbMulti.AutoSize = true;
+            this.rbMulti.Location = new System.Drawing.Point(6, 73);
+            this.rbMulti.Name = "rbMulti";
+            this.rbMulti.Size = new System.Drawing.Size(89, 17);
+            this.rbMulti.TabIndex = 8;
+            this.rbMulti.TabStop = true;
+            this.rbMulti.Text = "Multiplicación";
+            this.rbMulti.UseVisualStyleBackColor = true;
+            // 
+            // rbDivicion
+            // 
+            this.rbDivicion.AutoSize = true;
+            this.rbDivicion.Location = new System.Drawing.Point(6, 100);
+            this.rbDivicion.Name = "rbDivicion";
+            this.rbDivicion.Size = new System.Drawing.Size(63, 17);
+            this.rbDivicion.TabIndex = 9;
+            this.rbDivicion.TabStop = true;
+            this.rbDivicion.Text = "Divición";
+            this.rbDivicion.UseVisualStyleBackColor = true;
+            // 
+            // rbMin
+            // 
+            this.rbMin.AutoSize = true;
+            this.rbMin.Location = new System.Drawing.Point(6, 19);
+            this.rbMin.Name = "rbMin";
+            this.rbMin.Size = new System.Drawing.Size(58, 17);
+            this.rbMin.TabIndex = 10;
+            this.rbMin.TabStop = true;
+            this.rbMin.Text = "Minimo";
+            this.rbMin.UseVisualStyleBackColor = true;
+            // 
+            // rbMax
+            // 
+            this.rbMax.AutoSize = true;
+            this.rbMax.Location = new System.Drawing.Point(6, 38);
+            this.rbMax.Name = "rbMax";
+            this.rbMax.Size = new System.Drawing.Size(61, 17);
+            this.rbMax.TabIndex = 11;
+            this.rbMax.TabStop = true;
+            this.rbMax.Text = "Maximo";
+            this.rbMax.UseVisualStyleBackColor = true;
+            // 
+            // rbRC
+            // 
+            this.rbRC.AutoSize = true;
+            this.rbRC.Location = new System.Drawing.Point(6, 57);
+            this.rbRC.Name = "rbRC";
+            this.rbRC.Size = new System.Drawing.Size(97, 17);
+            this.rbRC.TabIndex = 12;
+            this.rbRC.TabStop = true;
+            this.rbRC.Text = "Raíz Cuadrada";
+            this.rbRC.UseVisualStyleBackColor = true;
+            // 
+            // rbPow
+            // 
+            this.rbPow.AutoSize = true;
+            this.rbPow.Location = new System.Drawing.Point(6, 76);
+            this.rbPow.Name = "rbPow";
+            this.rbPow.Size = new System.Drawing.Size(67, 17);
+            this.rbPow.TabIndex = 13;
+            this.rbPow.TabStop = true;
+            this.rbPow.Text = "Potencia";
+            this.rbPow.UseVisualStyleBackColor = true;
+            // 
+            // rbRedondeo
+            // 
+            this.rbRedondeo.AutoSize = true;
+            this.rbRedondeo.Location = new System.Drawing.Point(6, 95);
+            this.rbRedondeo.Name = "rbRedondeo";
+            this.rbRedondeo.Size = new System.Drawing.Size(75, 17);
+            this.rbRedondeo.TabIndex = 14;
+            this.rbRedondeo.TabStop = true;
+            this.rbRedondeo.Text = "Redondeo";
+            this.rbRedondeo.UseVisualStyleBackColor = true;
+            // 
+            // rbPI
+            // 
+            this.rbPI.AutoSize = true;
+            this.rbPI.Location = new System.Drawing.Point(6, 114);
+            this.rbPI.Name = "rbPI";
+            this.rbPI.Size = new System.Drawing.Size(86, 17);
+            this.rbPI.TabIndex = 15;
+            this.rbPI.TabStop = true;
+            this.rbPI.Text = "Constante PI";
+            this.rbPI.UseVisualStyleBackColor = true;
+            // 
+            // gbBasicas
+            // 
+            this.gbBasicas.Controls.Add(this.rbSuma);
+            this.gbBasicas.Controls.Add(this.rbResta);
+            this.gbBasicas.Controls.Add(this.rbMulti);
+            this.gbBasicas.Controls.Add(this.rbDivicion);
+            this.gbBasicas.Location = new System.Drawing.Point(12, 66);
+            this.gbBasicas.Name = "gbBasicas";
+            this.gbBasicas.Size = new System.Drawing.Size(106, 125);
+            this.gbBasicas.TabIndex = 16;
+            this.gbBasicas.TabStop = false;
+            this.gbBasicas.Text = "Op. Basicas";
+            // 
+            // gbAvanzadas
+            // 
+            this.gbAvanzadas.Controls.Add(this.rbMin);
+            this.gbAvanzadas.Controls.Add(this.rbMax);
+            this.gbAvanzadas.Controls.Add(this.rbPI);
+            this.gbAvanzadas.Controls.Add(this.rbRC);
+            this.gbAvanzadas.Controls.Add(this.rbRedondeo);
+            this.gbAvanzadas.Controls.Add(this.rbPow);
+            this.gbAvanzadas.Location = new System.Drawing.Point(124, 66);
+            this.gbAvanzadas.Name = "gbAvanzadas";
+            this.gbAvanzadas.Size = new System.Drawing.Size(111, 139);
+            this.gbAvanzadas.TabIndex = 10;
+            this.gbAvanzadas.TabStop = false;
+            this.gbAvanzadas.Text = "Op. Avanzadas";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnCalcular.FlatAppearance.BorderColor = System.Drawing.Color.BlueViolet;
+            this.btnCalcular.FlatAppearance.BorderSize = 0;
+            this.btnCalcular.FlatAppearance.MouseDownBackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnCalcular.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSteelBlue;
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalcular.Location = new System.Drawing.Point(185, 11);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 17;
+            this.btnCalcular.Text = "&Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnLimpiar.FlatAppearance.BorderColor = System.Drawing.Color.BlueViolet;
+            this.btnLimpiar.FlatAppearance.BorderSize = 0;
+            this.btnLimpiar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnLimpiar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSteelBlue;
+            this.btnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpiar.Location = new System.Drawing.Point(185, 40);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 18;
+            this.btnLimpiar.Text = "&Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnSalir.FlatAppearance.BorderColor = System.Drawing.Color.BlueViolet;
+            this.btnSalir.FlatAppearance.BorderSize = 0;
+            this.btnSalir.FlatAppearance.MouseDownBackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnSalir.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSteelBlue;
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalir.Location = new System.Drawing.Point(219, 258);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(41, 23);
+            this.btnSalir.TabIndex = 19;
+            this.btnSalir.Text = "&Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(59, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "________________";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(58, 41);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 13);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "________________";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.BlueViolet;
+            this.ClientSize = new System.Drawing.Size(272, 293);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.gbAvanzadas);
+            this.Controls.Add(this.gbBasicas);
+            this.Controls.Add(this.txtDato2);
+            this.Controls.Add(this.txtDato1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Calculadora Mamalona";
+            this.gbBasicas.ResumeLayout(false);
+            this.gbBasicas.PerformLayout();
+            this.gbAvanzadas.ResumeLayout(false);
+            this.gbAvanzadas.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDato1;
+        private System.Windows.Forms.TextBox txtDato2;
+        private System.Windows.Forms.RadioButton rbSuma;
+        private System.Windows.Forms.RadioButton rbResta;
+        private System.Windows.Forms.RadioButton rbMulti;
+        private System.Windows.Forms.RadioButton rbDivicion;
+        private System.Windows.Forms.RadioButton rbMin;
+        private System.Windows.Forms.RadioButton rbMax;
+        private System.Windows.Forms.RadioButton rbRC;
+        private System.Windows.Forms.RadioButton rbPow;
+        private System.Windows.Forms.RadioButton rbRedondeo;
+        private System.Windows.Forms.RadioButton rbPI;
+        private System.Windows.Forms.GroupBox gbBasicas;
+        private System.Windows.Forms.GroupBox gbAvanzadas;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Practica_1/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (txtDato1.Text == "" || txtDato2.Text == "")
+            {
+                MessageBox.Show("Ingrese los valores en los campos deseados");
+            }
+            double suma, resta, multi, divicion, min, max, rc, pow, redoneo, total;
+            double d1 = double.Parse(txtDato1.Text), d2 = double.Parse(txtDato2.Text);
+            total = suma = d1 + d2;
+            total = resta = d1 - d2;
+            total = multi = d1 * d2;
+            total = divicion = d1 / d2;
+            min = Math.Min(d1, d2);
+            max = Math.Max(d1, d2);
+            rc = Math.Sqrt(total);
+            pow = Math.Pow(d1, d2);
+            redoneo = Math.Round(total);
+
+            if (rbSuma.Checked == true)
+            {
+                label3.Text = "El resultado de la suma es: " + suma;
+            }
+            if (rbResta.Checked == true)
+            {
+                label3.Text = "El resultado de la resta es: " + resta;
+            }
+            if (rbMulti.Checked == true)
+            {
+                label3.Text = "El resultado de la multiplicación es: " + multi;
+            }
+            if (rbDivicion.Checked == true)
+            {
+                label3.Text = "El resultado de la divición es: " + divicion;
+            }
+            if (rbMin.Checked == true)
+            {
+                label4.Text = "El valor minimo es: " + min;
+            }
+            if (rbMax.Checked == true)
+            {
+                label4.Text = "El valor maximo es: " + max;
+            }
+            if (rbRC.Checked == true)
+            {
+                label4.Text = "El resultado de la raiz es: " + rc;
+            }
+            if (rbPow.Checked == true)
+            {
+                label4.Text = "EL valor " + d1 + " potenciado a " + d2 + " es: " + pow;
+            }
+            if (rbRedondeo.Checked == true)
+            {
+                label4.Text = "El valor redondeado es: " + redoneo;
+            }
+            if (rbPI.Checked == true)
+            {
+                label4.Text = "La constante de PI es: " + Math.PI;
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtDato1.Clear();
+            txtDato2.Clear();
+            rbSuma.Checked = rbResta.Checked = rbMulti.Checked = rbDivicion.Checked = rbMin.Checked = 
+                rbMax.Checked = rbRC.Checked = rbPow.Checked = rbRedondeo.Checked = rbPI.Checked = false;
+            label3.Text = "";
+            label4.Text = "";
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
